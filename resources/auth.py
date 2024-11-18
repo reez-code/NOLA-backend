6160b40bb3177a894dbbcd30569001ba765416d3
Phil <phil.github@gmail.com>
2024-11-18 22:05:41 +0300

    additional arguments for Client and Developer added

--- resources/auth.py
+++ resources/auth.py
@@ -11,8 +11,16 @@ class SignupResource(Resource):
     parser.add_argument('password', required=True, help='Password is required')
     parser.add_argument('role', required=True, help='Role is required (must be either developer, client or admin)')
 
-    # additional fields: Client
+    # additional arguments: Client
     parser.add_argument('business_name', help='business_name is required for clients')
     parser.add_argument('business_description', help='business_description is required for clients')
     parser.add_argument('logo', help='logo is required for clients')
     
+    # additional arguments: Developer
+    parser.add_argument("full_name", help="fullname is required for developers")
+    parser.add_argument("description", help="description is required for developers") 
+    parser.add_argument("github_account", help="github is required for developers")
+    parser.add_argument("profile_picture", help="profile_picture is required for developers")
+    parser.add_argument("skills", help="skills is required for developers")
+    parser.add_argument("education_level", help="education_level is required for developers")
+    parser.add_argument("available_time", help="available_time is required for developers")
