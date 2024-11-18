30a3650d66fb5cf5ac0ca035bfee71c13e24f5e3
Phil <phil.github@gmail.com>
2024-11-18 21:38:59 +0300

    added parser for compulsory fields in SignupResource

--- resources/auth.py
+++ resources/auth.py
@@ -1,3 +1,11 @@
 from flask_restful import Resource, reqparse
 from flask_bcrypt import generate_password_hash
 from flask_jwt_extended import create_access_token, jwt_required
+
+from models import User, DeveloperProfile, ClientProfile
+
+class SignupResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('email', required=True, help='Email is required')
+    parser.add_argument('username', required=True, help='First name is required')
+    parser.add_argument('password', required=True, help='Password is required')
