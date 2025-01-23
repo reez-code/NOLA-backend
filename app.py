694d1ac18df69eaf84a6336925c9c422128ac630
Phil <phil.github@gmail.com>
2025-01-23 10:20:07 +0300

    developer resources post method complete

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from config import app, api
 from resources.auth import Signup, Login, Logout
 from resources.client import ClientDetails
 from resources.job import JobResource
+from resources.developer import DeveloperDetails
 
 api.add_resource(Signup, "/signup", endpoint="signup")
 api.add_resource(Login, "/login", endpoint="login")
 api.add_resource(Logout, "/logout", endpoint="logout")
 api.add_resource(ClientDetails, "/client_details", endpoint="clientdetails")
 api.add_resource(JobResource, "/job", endpoint="job")
-
+api.add_resource(DeveloperDetails, "/developer_details", endpoint="developerdetails")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
---
+++ resources/developer.py
@@ -0,0 +1,50 @@
+from flask import make_response
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
+
+from models import DeveloperProfile, User
+from config import db
+
+class DeveloperDetails(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("first_name", required=True, help="First name is required")
+    parser.add_argument("last_name", required=True, help="Last name is required")
+    parser.add_argument("description", required=True, help="Description is required")
+    parser.add_argument("skills", required=True, help="Skills is required")
+    parser.add_argument("available_time", required=True, help="Available time is required")
+    parser.add_argument('github_account', required=True, help="Link to Github Account is required")
+    parser.add_argument("education_level", required=True, help="Education Level is required")
+    parser.add_argument("profile_picture", required=True, help="Profile Picture is required")
+
+    @jwt_required()
+    def post(self):
+        jwt = get_jwt()
+        if jwt["role"] in ["developer"]:
+            data = self.parser.parse_args()
+            try:
+                user_id = get_jwt_identity()
+                developer = DeveloperProfile(
+                    first_name=data["first_name"],
+                    last_name=data["last_name"],
+                    description=data["description"],
+                    skills=data["skills"],
+                    available_time=data["available_time"],
+                    github_account=data["github_account"],
+                    education_level=data["education_level"],
+                    profile_picture=data["profile_picture"],
+                    user_id=user_id
+                )
+                db.session.add(developer)
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                response =  {"errors": [str(e)], "status":"fail", "message":"Something went wrong"}
+                return make_response(response, 422)
+            
+            response = {"message": "Details added successfully",
+                        "status": "success",
+                        "developer_details": developer.to_dict()}
+            return make_response(response, 201)
+        else:
+            response = {"message": "You are not allowed access", "status": "fail"}
+            return make_response(response, 422)
