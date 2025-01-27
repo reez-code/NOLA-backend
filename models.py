40daeffff353d6ffeb1cb495b1584fc19aea6f79
Phil <phil.github@gmail.com>
2025-01-27 15:59:20 +0300

    updated models.py

--- models.py
+++ models.py
@@ -88,8 +88,8 @@ class ClientProfile(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True, nullable=False)
-    business_name = db.Column(db.String(150), nullable=False)
-    business_description = db.Column(db.Text, nullable=False)
+    business_name = db.Column(db.String(150), nullable=True)
+    business_description = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
 
